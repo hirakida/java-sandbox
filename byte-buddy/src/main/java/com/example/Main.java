8d57be09d9cfd340ff04bb4cb8b6598c08449f55
hirakida <hirakida29@gmail.com>
2020-10-11 20:04:59 +0900

    Add byte-buddy

---
+++ byte-buddy/src/main/java/com/example/Main.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import java.util.function.Function;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassLoader classLoader = Main.class.getClassLoader();
+
+        Class<?> dynamicType1 = new ByteBuddy()
+                .subclass(Object.class)
+                .method(ElementMatchers.named("toString"))
+                .intercept(FixedValue.value("Hello World!"))
+                .make()
+                .load(classLoader)
+                .getLoaded();
+
+        try {
+            Object instance = dynamicType1.getDeclaredConstructor().newInstance();
+            System.out.println(instance);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Class<? extends Function> dynamicType2 = new ByteBuddy()
+                .subclass(Function.class)
+                .method(ElementMatchers.named("apply"))
+                .intercept(MethodDelegation.to(new GreetingInterceptor()))
+                .make()
+                .load(classLoader)
+                .getLoaded();
+
+        try {
+            @SuppressWarnings("unchecked")
+            Function<String, String> instance = dynamicType2.getDeclaredConstructor().newInstance();
+            System.out.println(instance.apply("Byte Buddy"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class GreetingInterceptor {
+        public Object greet(Object argument) {
+            return "Hello from " + argument;
+        }
+    }
+}
