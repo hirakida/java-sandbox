8251835a115c491d70af4ed921d2090f735839ff
hirakida <hirakida29@gmail.com>
2021-01-02 15:50:23 +0900

    Update

--- rxjava/src/test/java/com/example/FlowableTest.java
+++ rxjava/src/test/java/com/example/FlowableTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import io.reactivex.Flowable;
 import io.reactivex.flowables.ConnectableFlowable;
 import io.reactivex.schedulers.Schedulers;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class FlowableTest {
 
     @Test
@@ -23,20 +25,20 @@ public class FlowableTest {
         Flowable.generate(emitter -> emitter.onNext(Math.random()))
                 .limit(5)
                 .cast(Double.class)
-                .subscribe(item -> System.out.println("onNext: " + item),
-                           e -> System.out.println("onError"),
-                           () -> System.out.println("onComplete"));
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
     }
 
     @Test
     public void merge() throws Exception {
         Flowable.merge(Flowable.range(1, 5)
-                               .delay(10, TimeUnit.MILLISECONDS),
+                               .subscribeOn(Schedulers.newThread()),
                        Flowable.range(6, 5)
-                               .delay(10, TimeUnit.MILLISECONDS))
-                .subscribe(item -> System.out.println("onNext: " + item),
-                           e -> System.out.println("onError"),
-                           () -> System.out.println("onComplete"));
+                               .subscribeOn(Schedulers.newThread()))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -46,31 +48,31 @@ public class FlowableTest {
         Flowable.zip(Flowable.range(1, 5),
                      Flowable.range(1, 5),
                      Integer::sum)
-                .subscribe(item -> System.out.println("onNext: " + item),
-                           e -> System.out.println("onError"),
-                           () -> System.out.println("onComplete"));
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
     }
 
     @Test
     public void startWith() {
         Flowable.range(6, 5)
                 .startWith(Flowable.range(1, 5))
-                .subscribe(item -> System.out.println("onNext: " + item),
-                           e -> System.out.println("onError"),
-                           () -> System.out.println("onComplete"));
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
     }
 
     @Test
     public void concat() throws Exception {
         Flowable.concat(Flowable.range(1, 5)
                                 .subscribeOn(Schedulers.newThread())
-                                .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)),
+                                .doOnNext(item -> log.info("doOnNext: {}", item)),
                         Flowable.range(6, 5)
                                 .subscribeOn(Schedulers.newThread())
-                                .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)))
-                .subscribe(item -> System.out.println(threadName() + " onNext: " + item),
-                           e -> System.out.println(threadName() + " onError"),
-                           () -> System.out.println(threadName() + " onComplete"));
+                                .doOnNext(item -> log.info("doOnNext: {}", item)))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -80,13 +82,13 @@ public class FlowableTest {
         Flowable.concatEager(
                 List.of(Flowable.range(1, 5)
                                 .subscribeOn(Schedulers.newThread())
-                                .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)),
+                                .doOnNext(item -> log.info("doOnNext: {}", item)),
                         Flowable.range(6, 5)
                                 .subscribeOn(Schedulers.newThread())
-                                .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i))))
-                .subscribe(item -> System.out.println(threadName() + " onNext: " + item),
-                           e -> System.out.println(threadName() + " onError"),
-                           () -> System.out.println(threadName() + " onComplete"));
+                                .doOnNext(item -> log.info("doOnNext: {}", item))))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -95,13 +97,13 @@ public class FlowableTest {
     public void concatArrayEager() throws Exception {
         Flowable.concatArrayEager(Flowable.range(1, 5)
                                           .subscribeOn(Schedulers.newThread())
-                                          .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)),
+                                          .doOnNext(item -> log.info("doOnNext: {}", item)),
                                   Flowable.range(6, 5)
                                           .subscribeOn(Schedulers.newThread())
-                                          .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)))
-                .subscribe(item -> System.out.println(threadName() + " onNext: " + item),
-                           e -> System.out.println(threadName() + " onError"),
-                           () -> System.out.println(threadName() + " onComplete"));
+                                          .doOnNext(item -> log.info("doOnNext: {}", item)))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -111,10 +113,10 @@ public class FlowableTest {
         Flowable.range(1, 5)
                 .flatMap(item -> Flowable.just(item)
                                          .subscribeOn(Schedulers.newThread())
-                                         .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)))
-                .subscribe(item -> System.out.println(threadName() + " onNext: " + item),
-                           e -> System.out.println(threadName() + " onError"),
-                           () -> System.out.println(threadName() + " onComplete"));
+                                         .doOnNext(item2 -> log.info("doOnNext: {}", item2)))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -124,10 +126,10 @@ public class FlowableTest {
         Flowable.range(1, 5)
                 .concatMap(item -> Flowable.just(item)
                                            .subscribeOn(Schedulers.newThread())
-                                           .doOnNext(i -> System.out.println(threadName() + " doOnNext: " + i)))
-                .subscribe(item -> System.out.println(threadName() + " onNext: " + item),
-                           e -> System.out.println(threadName() + " onError"),
-                           () -> System.out.println(threadName() + " onComplete"));
+                                           .doOnNext(item2 -> log.info("doOnNext: {}", item2)))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -137,11 +139,10 @@ public class FlowableTest {
         Flowable.range(1, 5)
                 .concatMapEager(item -> Flowable.just(item)
                                                 .subscribeOn(Schedulers.newThread())
-                                                .doOnNext(i -> System.out.println(threadName()
-                                                                                  + " doOnNext: " + i)))
-                .subscribe(item -> System.out.println(threadName() + " onNext: " + item),
-                           e -> System.out.println(threadName() + " onError"),
-                           () -> System.out.println(threadName() + " onComplete"));
+                                                .doOnNext(item2 -> log.info("doOnNext: {}", item2)))
+                .subscribe(item -> log.info("onNext: {}", item),
+                           e -> log.error("onError", e),
+                           () -> log.info("onComplete"));
 
         TimeUnit.MILLISECONDS.sleep(100);
     }
@@ -153,14 +154,10 @@ public class FlowableTest {
                                                            .publish();
         flowable.connect();
 
-        flowable.subscribe(item -> System.out.println(threadName() + " onNext1: " + item));
+        flowable.subscribe(item -> log.info("onNext1: {}", item));
         TimeUnit.MILLISECONDS.sleep(50);
-        flowable.subscribe(item -> System.out.println(threadName() + " onNext2: " + item));
+        flowable.subscribe(item -> log.info("onNext2: {}", item));
 
         TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    private static String threadName() {
-        return Thread.currentThread().getName();
     }
 }
